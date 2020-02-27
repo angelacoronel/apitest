3ddf8e43357df9d846e290039d197e2bddf87b63
Jella <53487935+angelacoronel@users.noreply.github.com>
2020-02-27 11:06:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $module = (isset($_GET['module']) && $_GET['module'] != '') ? $_GET['module'] : 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
   <title>API test</title>
 </head>
 <body>
