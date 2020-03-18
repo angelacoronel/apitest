9131de53326946afb8e50d7996c11f29219a52ad
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-18 16:45:32 +0800

    Update config.php

--- google/config.php
+++ google/config.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 $google_client->setClientId('404566657343-8vuefhffu82dik7hg7g1g6lukmo3dr53.apps.googleusercontent.com');
 $google_client->setClientSecret('C6-WypdO-TuLJXNY1JCPQCRI');
-$google_client->setRedirectUri('https://localhost/apitest/index.php');
+$google_client->setRedirectUri('https://apitest-ledesma.herokuapp.com/');
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
