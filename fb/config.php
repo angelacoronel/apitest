ff6a4b54633d78a984bba254790e8e642a237ef7
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-18 17:38:44 +0800

    Update config.php

--- fb/config.php
+++ fb/config.php
@@ -1,4 +1,4 @@
-<?php //------ credits: john paul maja :D <3 
+<?php
 
 require_once 'vendor/autoload.php';
 
@@ -7,11 +7,9 @@ if (!session_id())
     session_start();
 }
 
-// Call Facebook API 
-
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '568286157120937',
-  'app_secret'     => '1e3a40ac75b79a33fbf48f70731d4616',
+  'app_id'      => '1485657388278332',
+  'app_secret'     => 'b409862b4dcb94ff4238acae4fc71939',
   'default_graph_version'  => 'v2.10'
 ]);
 
